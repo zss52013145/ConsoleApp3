9671e71a30adc9c762b770cc3cfe0c1f6bc84561
菲菲 <lovezhangsan@outlook.com>
2022-04-11 11:12:24 +0800

    第三次修改

---
+++ ConsoleApp3/T1.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp3
+{
+    public class T1
+    {
+        public  string Name { get; set; }
+    }
+}
